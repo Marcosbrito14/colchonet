37b0c84f5fe5ff7b2c2c8bc5c8538efd749569c9
Marcos Brito <marcoslbrito@homail.com>
2022-08-15 20:19:42 -0300

    update i18n

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,6 @@
 class Room < ApplicationRecord
   validates_presence_of :title, :location
-  validates_length_of :description, minimum: 10, allow_blank: false, :message => "preencha essa bagaça"
+  validates_length_of :description, minimum: 10, allow_blank: false, :message => "Atenção aos erros"
 
   def complete_name
     "#{title}, #{location}"
